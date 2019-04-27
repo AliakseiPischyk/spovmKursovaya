d4430f6c38c38704a0b6954832c18df24d07d49f
AliakseiPischyk <aliaksei.pischyk@gmail.com>
2019-04-27 08:31:07 +0300

    save for cuda

--- spovmKursovaya/Main.cpp
+++ spovmKursovaya/Main.cpp
@@ -1,18 +1,22 @@
 #include "DSVParser.h"
 #include "LinearRegressionCalculator.h"
 #include "PowerRegressionCalculator.h"
+#include "MultipleRegressionCalculator.h"
 
 
 int main() {
+
+	Matrix<float_t*> m;
 	
-	std::vector<double> depVars{ 183,168, 171, 178 };
-	std::vector<double> indepVars{ 83,71, 64, 69 };
+	using TYPE = float_t;
+	std::vector<TYPE> depVars{ 183,168, 171, 178 };
+	std::vector<TYPE> indepVars{ 83,71, 64, 69 };
 
-	PowerRegressionCalculator<double_t>::calculate(depVars, indepVars);
+	PowerRegressionCalculator<TYPE >::calculate(depVars, indepVars);
 
-	//std::map<std::string, std::vector<float_t>> myMap = DSVParser< ',', float_t>::read("C:\\Users\\Sergei\\Desktop\\file.txt");
+	std::map<std::string, std::vector<TYPE >> myMap = DSVParser< ',', float_t>::read("C:\\Users\\Sergei\\Desktop\\file.txt");
 
-	//std::cout << LinearRegressionCalculator::calcRegressionLinearEquation(depVars, indepVars).getGradient() << '\n';
-	//std::cout << LinearRegressionCalculator::calcRegressionLinearEquation(depVars, indepVars).getInterceptY()<< '\n';
+	std::cout << LinearRegressionCalculator<TYPE>::calcRegressionLinearEquation(depVars, indepVars).getGradient() << '\n';
+	std::cout << LinearRegressionCalculator<TYPE>::calcRegressionLinearEquation(depVars, indepVars).getInterceptY()<< '\n';
 	system("pause");
 }
---
+++ spovmKursovaya/MultipleRegressionCalculator.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <optional>
+#include <memory>
+
+#include "DataSizeNotEqualException.h"
+
+template<class data_t, class = std::enable_if_t<std::is_pointer_v<data_t>>>
+class Matrix {
+
+private:
+	std::unique_ptr<data_t> begin;
+	size_t rows;
+	size_t columns;
+
+	static bool correctSizesPassed(const std::vector<std::remove_pointer<data_t>>& dependentData,
+		const std::vector<std::vector<std::remove_pointer<data_t>>>& independentData)
+		throw(DataSizeNotEqualException){
+
+		const size_t dataSize = dependentData.size();
+		for (const auto& column : independentData) {
+			if (column.size() != dataSize) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+public:
+
+	Matrix() {};
+
+	Matrix(const std::vector<std::remove_pointer<data_t>>& dependentData,
+		const std::vector<std::vector<std::remove_pointer<data_t>>>& independentData ) 
+		throw(DataSizeNotEqualException){
+		
+		if (!correctSizesPassed(dependentData, independentData)) {
+			throw DataSizeNotEqualException;
+		}
+		////////////////////////////
+	}
+
+	static void multiply(const Matrix<data_t>& source1, 
+		const Matrix<data_t>& source2, 
+		Matrix<data_t>& dest) {
+
+	}
+
+	static void transpose(const Matrix<data_t>& source,
+		Matrix<data_t>& dest) {
+
+	}
+
+	static void inverse(const Matrix<data_t>& source,
+		Matrix<data_t>& dest) {
+
+	}
+
+	~Matrix() {
+		free(begin.get());
+	}
+};
+
+template<class data_t, class = std::enable_if_t<std::is_floating_point_v<data_t>>>
+class MultipleRegressionCalculator
+{
+public:
+	using data_t_ptr = std::add_pointer_t<data_t>;
+
+	static bool calc(const Matrix<data_t_ptr>& dependentData,
+		const Matrix<data_t_ptr>& independentData) {
+
+		Matrix<data_t_ptr> transIndep;
+		Matrix<data_t_ptr>::transpose(independentData, transIndep);
+
+		Matrix<data_t_ptr> multTransIndepByIndep;
+		Matrix<data_t_ptr>::multiply(transIndep, independentData, multTransIndepByIndep);
+
+		Matrix<data_t_ptr> multTransIndepByDep;
+		Matrix<data_t_ptr>::multiply(transIndep, dependentData, multTransIndepByDep);
+
+		Matrix<data_t_ptr> inserveMultTransIndepByIndep;
+		Matrix<data_t_ptr>::inverse(multTransIndepByIndep, inserveMultTransIndepByIndep);
+
+		Matrix<data_t_ptr> equationCoefficients;
+		Matrix<data_t_ptr>::multiply(inserveMultTransIndepByIndep, multTransIndepByDep, equationCoefficients);
+	}
+};
+
